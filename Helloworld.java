96a4d0570fcba13ee9b126532cb9223e3b86d9e3
Saurav <dac@students.acts.in>
2024-05-28 09:30:02 +0530

    A small change made here to B1 statement

--- Helloworld.java
+++ Helloworld.java
@@ -2,6 +2,6 @@ class Helloworld {
     public static void main(String args[]) {
         System.out.println("Welcome to New World!");
         System.out.println("Changes made by B-2!");
-        System.out.println("Changed by B1!");
+        System.out.println("Changes made by B1!");
     }
 }
